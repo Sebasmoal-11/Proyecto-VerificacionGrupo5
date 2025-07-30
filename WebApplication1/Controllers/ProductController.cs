6d79389411fceb7bdeb8e5c96747b0d2b268518b
Sebasmoal-11 <sebastianmoraalvarado12@gmail.com>
2025-07-30 14:43:08 -0600

    Agregar archivos de proyecto.

---
+++ WebApplication1/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TiendaVirtualMVC.Models;
+using System.Collections.Generic;
+
+namespace TiendaVirtualMVC.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            var products = new List<Product>
+            {
+                new Product { Name = "Producto 1", Description = "Descripción 1", Price = 10 },
+                new Product { Name = "Producto 2", Description = "Descripción 2", Price = 15 }
+            };
+
+            return View(products);
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                // Aquí podrías guardar el producto en una lista o base de datos
+                return RedirectToAction("Index");
+            }
+
+            return View(product);
+        }
+    }
+}
+
