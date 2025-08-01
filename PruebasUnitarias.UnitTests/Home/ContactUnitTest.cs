4b99f80cbf93585440c92359aefbd0aec5394092
Jorshua Mena Rivera <jorshua16_mr@hotmail.com>
2025-07-31 19:29:09 -0600

    Se agrego el proyecto de pruebas UniTests. Se agrego la carpeta Home junto con el Contact para la prueba 9. Pendiente Error en ContactUnitTest.

---
+++ PruebasUnitarias.UnitTests/Home/ContactUnitTest.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PruebasUnitarias.UnitTests.Home
+{
+    [TestClass]
+    public class ContactUnitTest
+    {
+        private (bool, string) resultadoEsperado;
+        private (bool, string) resultadoObtenido;
+        private Contact contact;
+
+        [TestMethod]
+        [TestCategory("Probar la validación de los campos del formulario de contacto")] //nombre del requerimiento 9
+
+        public void losCamposSonValidos_SonValidos()
+        {
+            this.contact = new Contact();
+            {
+
+            }
+        }
+
+
+    }
+}
