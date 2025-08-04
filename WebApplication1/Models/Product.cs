bf2a9b7f0f9e0709839cc2fdb329ba683afd32a2
Sebasmoal-11 <sebastianmoraalvarado12@gmail.com>
2025-08-04 15:55:12 -0600

    Agregado validaciones en Product y pruebas unitarias para requerimientos 3 y 6.

--- WebApplication1/Models/Product.cs
+++ WebApplication1/Models/Product.cs
@@ -1,10 +1,16 @@
-﻿namespace TiendaVirtualMVC.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TiendaVirtualMVC.Models
 {
     public class Product
     {
+        [Required(ErrorMessage = "El nombre es obligatorio")]
         public string Name { get; set; }
+
+        [Required(ErrorMessage = "La descripción es obligatoria")]
         public string Description { get; set; }
+
+        [Range(0.01, double.MaxValue, ErrorMessage = "El precio debe ser mayor a 0")]
         public decimal Price { get; set; }
     }
 }
-
