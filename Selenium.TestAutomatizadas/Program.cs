383f36752c1213793c2405bffcb8ab7024d4bc11
Sebasmoal-11 <sebastianmoraalvarado12@gmail.com>
2025-08-06 16:09:14 -0600

    Creacion del proyecto para pruebas automatizadas y se quito la carpeta de pruebas

---
+++ Selenium.TestAutomatizadas/Program.cs
@@ -0,0 +1,21 @@
+﻿
+namespace Selenium.TestAutomatizadas
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            string rutaWindows = @""; //aca ponen su ruta donde tienen guardada la pagina web
+            string rutaUrl = new Uri(rutaWindows).AbsoluteUri;
+
+            Console.WriteLine(rutaUrl);
+
+            string urlFormulario = rutaUrl;
+
+            var auto = new FormularioTest(urlFormulario);
+
+            auto.Ejecutar();
+        }
+    }
+}
